cae914caf1fc3be22a77291825cfdac1883b04fd
kPherox <admin@mail.kr-kp.com>
2019-12-27 16:34:55 +0900

    Add test for Client fallback

---
+++ Tests/SocialProviderTests/ClientTests.swift
@@ -0,0 +1,137 @@
+import XCTest
+@testable import SocialProtocol
+
+private struct TestAccount: Account {
+
+    static var provider: Provider = Provider(rawValue: "")
+
+    let id: String = ""
+    let name: String = ""
+    let username: String = ""
+    let avaterUrl: URL = URL(string: "https://pl.kpherox.dev/images/avi.png")!
+
+}
+
+private struct TestCredentials: Credentials, OAuth2 {
+
+    let apiKey: String = ""
+    let apiSecret: String = ""
+    let oauthToken: String = ""
+
+}
+
+private class TestClient: Client {
+
+    let credentials: Credentials
+    var userAgent: String?
+
+    required init?(_ credentials: Credentials, userAgent: String?) {
+        self.credentials = credentials
+        self.userAgent = userAgent
+    }
+
+    func revoke(success: Client.Success?, failure: Client.Failure?) {
+        XCTAssertNil(success)
+        XCTAssertNil(failure)
+    }
+
+    func verify(success: @escaping Client.AccountSuccess, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+    func post(text: String, otherParams: [String : String]?, success: Client.Success?, failure: Client.Failure?) {
+        XCTAssertNil(otherParams)
+    }
+
+}
+
+extension TestClient: D14nClient {
+
+    static func registerApp(base: String, name: String, redirectUri: String, success: @escaping D14nClient.RegisterSuccess, failure: Client.Failure?) {
+        XCTAssertEqual(redirectUri, "urn:ietf:wg:oauth:2.0:oob")
+    }
+
+}
+
+extension TestClient: PostAttachments {
+
+    func post(text: String, image: Data?, otherParams: [String : String]?, success: Client.Success?, failure: Client.Failure?) {
+        XCTAssertNil(image)
+        XCTAssertNil(otherParams)
+    }
+
+}
+
+extension TestClient: AuthorizeByCallback, AuthorizeByCode {
+
+    static func handleCallback(_: NSAppleEventDescriptor) {}
+
+    static func authorize(key: String, secret: String, redirectUri: String, success: @escaping Client.TokenSuccess, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+    static func authorize(key: String, secret: String, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+    static func requestToken(key: String, secret: String, code: String, success: @escaping Client.TokenSuccess, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+}
+
+extension TestClient: D14nAuthorizeByCallback, D14nAuthorizeByCode {
+
+    static func authorize(base: String, key: String, secret: String, redirectUri: String, success: @escaping Client.TokenSuccess, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+    static func authorize(base: String, key: String, secret: String, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+    static func requestToken(base: String, key: String, secret: String, code: String, success: @escaping Client.TokenSuccess, failure: Client.Failure?) {
+        XCTAssertNil(failure)
+    }
+
+}
+
+final class ClientTests: XCTestCase {
+
+    static var allTests = [
+        ("Test fallback Client initalizer", testFallbackInitializer),
+        ("Test fallback Client functions", testFallbackFunctions),
+        ("Test fallback Client siatic functions", testFallbackStaticFunctions),
+    ]
+
+    func testFallbackInitializer() {
+        let client = TestClient(TestCredentials())
+        XCTAssertNil(client?.userAgent)
+    }
+
+    func testFallbackFunctions() {
+        let client = TestClient(TestCredentials(), userAgent: nil)!
+        client.revoke()
+        client.verify(success: { _ in })
+        client.post(text: "")
+
+        // PostAttachments
+        client.post(text: "", image: nil)
+    }
+
+    func testFallbackStaticFunctions() {
+        // D14nClient
+        TestClient.registerApp(base: "", name: "", success: { _,_ in })
+
+        // AuthorizeByCallback, D14nAuthorizeByCallback
+        TestClient.authorize(key: "", secret: "", redirectUri: "", success: { _ in })
+        TestClient.authorize(base: "", key: "", secret: "", redirectUri: "", success: { _ in })
+
+        // AuthorizeByCode, D14nAuthorizeByCode
+        TestClient.authorize(key: "", secret: "")
+        TestClient.authorize(base: "", key: "", secret: "")
+        TestClient.requestToken(key: "", secret: "", code: "", success: { _ in })
+        TestClient.requestToken(base: "", key: "", secret: "", code: "", success: { _ in })
+    }
+
+}
--- Tests/SocialProviderTests/XCTestManifests.swift
+++ Tests/SocialProviderTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(AccountTests.allTests),
+        testCase(ClientTests.allTests),
         testCase(CredentialsTests.allTests),
         testCase(ProviderTests.allTests),
         testCase(SocialErrorTests.allTests),
