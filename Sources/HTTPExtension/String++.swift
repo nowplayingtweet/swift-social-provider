544195e6df831acd3981e93417244f4b56924f62
kPherox <admin@mail.kr-kp.com>
2019-12-29 04:16:45 +0900

    Fix string to query components

--- Sources/HTTPExtension/String++.swift
+++ Sources/HTTPExtension/String++.swift
@@ -11,10 +11,12 @@ public extension String {
 
     var queryParamComponents: [String : String] {
         return self.components(separatedBy: "&").map({
-            $0.components(separatedBy: "=")
+            $0.components(separatedBy: "=").map({ $0.removingPercentEncoding! })
         }).reduce(into: [String:String]()) { dict, pair in
             if pair.count == 2 {
                 dict[pair[0]] = pair[1]
+            } else if pair.count == 1 {
+                dict[pair[0]] = ""
             }
         }
     }
