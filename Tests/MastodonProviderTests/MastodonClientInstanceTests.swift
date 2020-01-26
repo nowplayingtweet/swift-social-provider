ca0fc8c6560eedd18e3f29fe73658a8a4460827f
kPherox <admin@mail.kr-kp.com>
2020-01-27 03:09:47 +0900

    Add test for request token

--- Tests/MastodonProviderTests/MastodonClientInstanceTests.swift
+++ Tests/MastodonProviderTests/MastodonClientInstanceTests.swift
@@ -49,4 +49,88 @@ final class MastodonClientInstanceTests: XCTestCase {
         })
     }
 
+    func testRequestToken() {
+        let httpClient = HTTPClientMock(body: """
+        {
+            "access_token": "authorization-token",
+            "token_type": "Bearer",
+            "scope": "read write",
+            "created_at": 1234567890
+        }
+        """.data(using: .utf8))
+        let client = MastodonClient(base: "https://social.test", key: "key", secret: "secret", httpClient: httpClient)
+        XCTAssertNotNil(client)
+
+        client?.requestToken(code: "code", success: { credentials in
+            guard let credentials = credentials as? MastodonCredentials
+                , let clientCredentials = client?.credentials as? MastodonCredentials else {
+                    XCTFail()
+                    return
+            }
+            XCTAssertEqual(credentials.base, "https://social.test")
+            XCTAssertEqual(credentials.apiKey, "key")
+            XCTAssertEqual(credentials.apiSecret, "secret")
+            XCTAssertEqual(credentials.oauthToken, "authorization-token")
+            XCTAssertEqual(clientCredentials.oauthToken, "authorization-token")
+        }, failure: { _ in
+            XCTFail()
+        })
+    }
+
+    func testRequestTokenClientError() {
+        let responseError = URLError(.cannotConnectToHost)
+        let httpClient = HTTPClientMock(error: responseError)
+        let client = MastodonClient(base: "https://social.invalid", key: "key", secret: "secret", httpClient: httpClient)
+        XCTAssertNotNil(client)
+
+        client?.requestToken(code: "code", success: { _ in
+            XCTFail()
+        }, failure: { error in
+            XCTAssertEqual(error.localizedDescription, responseError.localizedDescription)
+        })
+    }
+
+    func testRequestTokenErrorResponse() {
+        let httpClient400 = HTTPClientMock(statusCode: 400, body: """
+        {
+            "error": "invalid_scope",
+            "error_description": "The requested scope is invalid, unknown, or malformed."
+        }
+        """.data(using: .utf8))
+        let httpClient401 = HTTPClientMock(statusCode: 401, body: """
+        {
+            "error": "invalid_client",
+            "error_description": "Client authentication failed due to unknown client, no client authentication included, or unsupported authentication method."
+        }
+        """.data(using: .utf8))
+
+        let client = MastodonClient(base: "https://social.invalid", key: "key", secret: "secret", httpClient: httpClient400)
+        XCTAssertNotNil(client)
+
+        client?.requestToken(code: "code", success: { _ in
+            XCTFail()
+        }, failure: { error in
+            XCTAssertEqual(error.localizedDescription, SocialError.failedAuthorize("400").localizedDescription)
+        })
+
+        client?.client = httpClient401
+        client?.requestToken(code: "code", success: { _ in
+            XCTFail()
+        }, failure: { error in
+            XCTAssertEqual(error.localizedDescription, SocialError.failedAuthorize("401").localizedDescription)
+        })
+    }
+
+    func testRequestTokenInvalidResponse() {
+        let httpClient = HTTPClientMock()
+        let client = MastodonClient(base: "https://social.invalid", key: "key", secret: "secret", httpClient: httpClient)
+        XCTAssertNotNil(client)
+
+        client?.requestToken(code: "code", success: { _ in
+            XCTFail()
+        }, failure: { error in
+            XCTAssertEqual(error.localizedDescription, SocialError.failedAuthorize("Invalid response.").localizedDescription)
+        })
+    }
+
 }
